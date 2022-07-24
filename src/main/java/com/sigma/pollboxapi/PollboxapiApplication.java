d5ef8686850f495ff42f95e01137b8dc83d42f9e
SigmaDevelopment2022 <sigma.dev.2022@gmail.com>
2022-07-24 17:12:37 -0300

    Primeiro ommit

---
+++ src/main/java/com/sigma/pollboxapi/PollboxapiApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.pollboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollboxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollboxapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigma/pollboxapi/PollboxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.pollboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollboxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
