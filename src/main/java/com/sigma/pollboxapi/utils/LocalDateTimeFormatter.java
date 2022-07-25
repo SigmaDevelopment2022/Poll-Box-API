26a9931260be0937be9a016f11627050e87a9204
skai7ami <skai7ami@gmail.com>
2022-07-25 13:21:14 -0300

    formatação corrigida

--- src/main/java/com/sigma/pollboxapi/utils/LocalDateTimeFormatter.java
+++ src/main/java/com/sigma/pollboxapi/utils/LocalDateTimeFormatter.java
@@ -1,20 +1,16 @@
 package com.sigma.pollboxapi.utils;
 
-import org.springframework.cglib.core.Local;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeFormatter {
-    public static LocalDateTime fromDatabase(String localDateTime) {
+    public static String fromDatabase(String localDateTime) {
         DateTimeFormatter databasePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        DateTimeFormatter brazilPattern = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        LocalDateTime databaseDateTime = LocalDateTime.parse(localDateTime, databasePattern);
-        return LocalDateTime.parse(databaseDateTime.toString(), brazilPattern);
+        DateTimeFormatter brazilianPattern = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return LocalDateTime.parse(localDateTime, databasePattern).format(brazilianPattern);
     }
 
-    public static LocalDateTime format(LocalDateTime localDateTime) {
-        DateTimeFormatter brazilPattern = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        return LocalDateTime.parse(localDateTime.toString(), brazilPattern);
+    public static String format(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
     }
 }
