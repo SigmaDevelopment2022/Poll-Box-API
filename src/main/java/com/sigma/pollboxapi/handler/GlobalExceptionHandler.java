75edeb07e7ba601844c53692d7a5b10311353976
skai7ami <skai7ami@gmail.com>
2022-07-25 11:06:48 -0300

    captura de exceções customizadas

--- src/main/java/com/sigma/pollboxapi/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sigma/pollboxapi/handler/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.sigma.pollboxapi.handler;
 
 import com.sigma.pollboxapi.exceptions.DuplicatedEntryException;
 import com.sigma.pollboxapi.exceptions.ExceptionDetails;
+import com.sigma.pollboxapi.exceptions.NotFoundException;
 import com.sigma.pollboxapi.utils.LocalDateTimeFormatter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,5 +22,25 @@ public class GlobalExceptionHandler {
                 .message(e.getMessage())
                 .status(HttpStatus.CONFLICT.value())
                 .build(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionDetails> handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity<>(ExceptionDetails.builder()
+                .datetime(LocalDateTimeFormatter.format(LocalDateTime.now()))
+                .title("Recurso não encontrado")
+                .message(e.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ExceptionDetails> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        return new ResponseEntity<>(ExceptionDetails.builder()
+                .datetime(LocalDateTimeFormatter.format(LocalDateTime.now()))
+                .title("Recurso não encontrado")
+                .message(e.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .build(), HttpStatus.NOT_FOUND);
     }
 }
