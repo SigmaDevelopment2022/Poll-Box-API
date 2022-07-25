76e80e125981e966abc15f0aa9548a05d5874443
skai7ami <skai7ami@gmail.com>
2022-07-25 10:44:02 -0300

    findUsingEmail e update

--- src/main/java/com/sigma/pollboxapi/daos/AccountDao.java
+++ src/main/java/com/sigma/pollboxapi/daos/AccountDao.java
@@ -2,8 +2,11 @@ package com.sigma.pollboxapi.daos;
 
 import com.sigma.pollboxapi.domain.Account;
 import com.sigma.pollboxapi.exceptions.DuplicatedEntryException;
+import com.sigma.pollboxapi.exceptions.NotFoundException;
+import com.sigma.pollboxapi.mappers.AccountMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -25,6 +28,28 @@ public class AccountDao {
                 throw new DuplicatedEntryException("Este e-mail já está em uso.");
             }
             throw e;
+        }
+    }
+
+    public Account findUsingEmail(String email, AccountMapper mapper) {
+        String statement = "select * from accounts where email=?";
+        try {
+            return template.queryForObject(statement, mapper, email);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public boolean update(Account account) {
+        String statement = "update accounts set username=?, password=?, authenticated=?, device_identification=? where email=?";
+        try {
+            return template.update(statement,
+                    account.getUsername(),
+                    account.getPassword(),
+                    account.isAuthenticated(),
+                    account.getEmail()) > 0;
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException("Não há usuário cadastrado com este e-mail.");
         }
     }
 }
