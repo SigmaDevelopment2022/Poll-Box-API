9da73351c11fb5ee9ef1e90b9602870fba484850
skai7ami <skai7ami@gmail.com>
2022-07-25 17:24:03 -0300

    modo de formatar datas alterado

--- src/main/java/com/sigma/pollboxapi/utils/LocalDateTimeFormatter.java
+++ src/main/java/com/sigma/pollboxapi/utils/LocalDateTimeFormatter.java
@@ -4,11 +4,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeFormatter {
-    public static String fromDatabase(String localDateTime) {
+    public static String brazilianFormat(String localDateTime) {
         DateTimeFormatter databasePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         DateTimeFormatter brazilianPattern = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         return LocalDateTime.parse(localDateTime, databasePattern).format(brazilianPattern);
     }
+
+    public static LocalDateTime fromDatabase(String localDateTime) {
+        return LocalDateTime.parse(localDateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 
     public static String format(LocalDateTime localDateTime) {
         return localDateTime.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
