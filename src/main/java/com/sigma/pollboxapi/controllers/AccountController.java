394efb2cedffcdbe3b7ed209c5998f955b771119
skai7ami <skai7ami@gmail.com>
2022-07-25 17:30:39 -0300

    geração de código

--- src/main/java/com/sigma/pollboxapi/controllers/AccountController.java
+++ src/main/java/com/sigma/pollboxapi/controllers/AccountController.java
@@ -3,23 +3,32 @@ package com.sigma.pollboxapi.controllers;
 import com.sigma.pollboxapi.domain.Account;
 import com.sigma.pollboxapi.requests.AccountSignupRequest;
 import com.sigma.pollboxapi.services.AccountService;
+import com.sigma.pollboxapi.services.CodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("accounts")
+@RequestMapping()
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final CodeService codeService;
 
-    @PostMapping("/signup")
+    @PostMapping("accounts/signup")
     public ResponseEntity<Account> signup(@RequestBody @Validated AccountSignupRequest request) {
         return new ResponseEntity<>(accountService.signup(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("admin/accounts/login")
+    public ResponseEntity<Account> login(@RequestParam(value = "device-identification") String deviceIdentification) {
+        return new ResponseEntity<>(accountService.login(deviceIdentification), HttpStatus.OK);
+    }
+
+    @PostMapping("admin/accounts/generate-code")
+    public ResponseEntity<Long> generateCode(@RequestParam String type) {
+        return new ResponseEntity<>(codeService.generateCode(type), HttpStatus.CREATED);
     }
 }
